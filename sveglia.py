d86d63e0ad2b07347838bc5d4f01b8cfed087be6
Seboscoo <sebasbosco@icloud.com>
2026-09-15 10:03:21 +0200

    Create sveglia.py

---
+++ sveglia.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+import time
+
+# INSERISCI QUI SOTTO IL LINK DEL TUO SITO
+URL = https://totocarnico.streamlit.app/
+options = webdriver.ChromeOptions()
+options.add_argument('--headless') # Fa girare il browser in background
+
+print("Avvio il browser invisibile...")
+driver = webdriver.Chrome(options=options)
+driver.get(URL)
+
+# Aspetta 10 secondi per far caricare la grafica di Streamlit
+time.sleep(10) 
+
+# Simula uno scroll della pagina verso il basso
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+print("Pagina visitata e scorsa con successo! Il sito è sveglio.")
+
+driver.quit()
